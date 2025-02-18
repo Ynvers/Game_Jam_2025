7f2c9e1eaa0f3daa6bb3c23c37e7d4a4167f9b1e
Ynvers <luzolo.adoho@gmail.com>
2025-02-18 17:54:41 +0100

    Mise à jour de la map

---
+++ Game/scripts/Player_main.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var speed = 200
+var direction = Vector2.ZERO
+
+func _physics_process(delta):
+	#Déplacement si il est sur mobile
+	direction = Vector2.ZERO
+	if Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down"):
+		direction.y = Input.get_axis("ui_up", "ui_down")
+	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
+		direction.x = Input.get_axis("ui_left", "ui_right")	
+	
+	#Normaise pour eviter d'aller plus vite en diagonale
+	direction = direction.normalized()
+	velocity = direction * speed
+	move_and_slide()
---
+++ Game/scripts/niveau1.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+@export var description = "Un niveau inconnu"
+
+func _ready():
+	# Assure-toi que chaque niveau est bien dans un groupe
+	print("Niveau chargé : ", name)
+	add_to_group("Niveaux")
+
+# Assombrit le niveau en baissant l'opacité
+func assombrir():
+	modulate = Color(0.5, 0.5, 0.5, 1)
+
+# Retourne la description du niveau
+func get_description():
+	return "🎮 " + description
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print(body.name)
+	if body.is_in_group("Players"):
+		print("Entré dans un niveau valide :", self.name)
+		assombrir()
+		
+		get_parent()
+		var feedback_label = get_parent().get_parent().get_node("FeedBack")
+		if feedback_label:
+			feedback_label.text = get_description()
+		else:
+			print("Le nœud FeedBack n'a pas été trouvé ou n'est pas un Label !")
+		load_level_after_delay()
+
+func load_level_after_delay():
+	await get_tree().create_timer(3.0).timeout
+	get_tree().change_scene_to_file("res://scenes/jupidon_lab.tscn")
